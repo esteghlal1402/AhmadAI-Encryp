cdf47453cb07489166fb510365e8cb4f50fd1df8
esteghlal1402 <202157739+esteghlal1402@users.noreply.github.com>
2025-09-03 14:53:18 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "📦 در حال نصب AhmadAl-Encrypt..."
+git clone https://github.com/AhmadAl/AhmadAl-Encrypt.git
+cd AhmadAl-Encrypt
+chmod +x encryptor.sh decryptor.sh
+echo "✅ نصب کامل شد! حالا می‌تونی اجرا کنی:"
+echo "bash encryptor.sh"
